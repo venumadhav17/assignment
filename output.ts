9448bb4aa03316a805b21b2e27c7f183c94c1fb4
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-05-17 16:52:28 +0530

    added backend prisma for database for combined data 1.  added express, prisma for db

---
+++ output.ts
@@ -0,0 +1,12 @@
+// We get the output like this
+
+{
+    "employees": [
+      { "id": 1, "firstName": "Venu", "lastName": "Madhav" },
+    ]
+
+    "products": [
+      { "id": 1, "productName": "Laptop", "cost", "price": 1000 },
+    ]
+  }
+  
